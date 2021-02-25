9e2a1949ecb75854d2e62c035fd7e3a18fbc4b0b
Wade-Philander <56066240+Wade-Philander@users.noreply.github.com>
2021-02-25 15:53:40 +0200

    Add files via upload

--- weather.py
+++ weather.py
@@ -1,7 +1,5 @@
-from flask import Flask
-import requests
+from flask import Flask, render_template, request, jsonify
 import sqlite3
-from flask import jsonify
 
 
 weather = Flask(__name__)
@@ -16,27 +14,80 @@ def create_database_tables():
         # cursor = connection.cursor()
         # cursor.execute('')
         # cursor.execute()
-        # cursor = sql.commit()
+        #cursor = sql.commit()
 
-@weather.route('/register-user/', methods=['GET'])
-def register():
+
+#*******************************************  ADD NEW USER  ******************************************************************
+
+
+@weather.route('/add-new-user/', methods=['POST'])
+def new_user():
+    if request.method =="POST":
+        
+        msg = None
+
+        try:
+            name = request.form['name']
+            username = request.form['username']
+            password = request.form['password']
+            city = request.form['city']
+
+            with sqlite3.connect('users.db') as con:
+                cur = con.cursor()
+                cur.execute("INSERT INTO users(name, username, password, city) VALUES (?, ?, ?, ?)", (name, username, password, city))
+                con.commit()
+                msg = name = "was successfully added to the database."
+                print("New user has been added")
+
+        except Exception as e:
+            con.rollback()
+            msg = "Error occurred: " +str(e)
+
+        finally:
+            con.close()
+            return jsonify (msg = msg)
+
+
+#**************************************************  SHOW USERS DATABASE  **************************************************************
+
+@weather.route('/show-users/', methods=["GET"])
+
+def show_users():
+    records = []
+    try:
+        with sqlite3.connect('users.db') as con:
+            cur = con.cursor()
+            cur.execute("SELECT * From student")
+            record = cur.fetchall()
+            print("Here are the users")
+
+    except Exception as e:
+        con.rollback()
+        print("There was an error fetching users: " +str(e))
+
+    finally:
+        con.close()
+        return jsonify (records)
+
 
 
 @weather.route('/login-user/', methods=['POST'])
-
 def login_user():
     # get form data
     # check db 
     pass
 
+
 #*******************************************************************************************************************************************************
+#*******************************************************************************************************************************************************
+
 
 @weather.route('/',methods=['GET'])
 def get_weather_today():
 
     api_url ="http://api.openweathermap.org/data/2.5/forecast?q=London&appid=e30a3785da67bd9d83b65bf8933359b5"
 
-    response= requests.get(api_url)
+    response= request.get(api_url)
     json_data=response.json()
 
     
